35e8c027cb021339ef94814d40f416ce5711be2e
Simon Smith <artificialsimon@gmail.com>
2018-12-10 19:03:49 +0000

    not resizing input

--- ga3c/Environment.py
+++ ga3c/Environment.py
@@ -55,7 +55,7 @@ class Environment:
     @staticmethod
     def _preprocess(image):
         image = Environment._rgb2gray(image)
-        image = misc.imresize(image, [Config.IMAGE_HEIGHT, Config.IMAGE_WIDTH], 'bilinear')
+        #image = misc.imresize(image, [Config.IMAGE_HEIGHT, Config.IMAGE_WIDTH], 'bilinear')
         image = image.astype(np.float32) / 128.0 - 1.0
         return image
 
@@ -71,8 +71,6 @@ class Environment:
             self.frame_q.get()
         image = Environment._preprocess(frame)
         self.frame_q.put(image)
-        #print(frame)
-        #self.frame_q.put(frame)
 
     def get_num_actions(self):
         return self.game.env.action_space.n

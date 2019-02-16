1d8a85715230554588d70541ad9b55a35686ed06
Simon Smith <artificialsimon@gmail.com>
2019-02-16 16:12:18 +0000

    test version for todor

--- ga3c/NetworkVP.py
+++ ga3c/NetworkVP.py
@@ -46,8 +46,9 @@ class NetworkVP:
         self.beta = Config.BETA_START
         self.log_epsilon = Config.LOG_EPSILON
 
+
         self.graph = tf.Graph()
-        with self.graph.as_default() as g:
+        with self.graph.as_default():
             with tf.device(self.device):
                 self._create_graph()
 
@@ -63,7 +64,105 @@ class NetworkVP:
                 if Config.LOAD_CHECKPOINT or Config.SAVE_MODELS:
                     vars = tf.global_variables()
                     self.saver = tf.train.Saver({var.name: var for var in vars}, max_to_keep=0)
-                
+
+        #self.graph_rise = tf.Graph()
+        #with self.graph_rise.as_default(), tf.device("gpu:0"):
+            #self._create_graph_scope()
+            #self.sess_rise = tf.Session(
+                #graph=self.graph_rise,
+                #config=tf.ConfigProto(
+                    #allow_soft_placement=True,
+                    #log_device_placement=False,
+                    #gpu_options=tf.GPUOptions(allow_growth=True)))
+            ##with self.sess_rise.as_default():
+            #self.sess_rise.run(tf.global_variables_initializer())
+            #vars = tf.global_variables()
+            #self.saver_rise = tf.train.Saver({var.name: var for var in vars}, max_to_keep=0)
+            #self.saver_rise.restore(self.sess_rise, 'checkpoints/risetotop/network_00003000')
+
+
+
+    def _create_graph_scope(self):
+        #with tf.name_scope("risetotop"):
+            #self._create_graph()
+            #self.saver.restore(self.sess, "checkpoints/risetotop/network_00002001")
+        self.x_rise = tf.placeholder(
+            tf.float32, [None, self.img_height, self.img_width, self.img_channels], name='X')
+        self.y_r_rise = tf.placeholder(tf.float32, [None], name='Yr')
+        self.var_beta_rise = tf.placeholder(tf.float32, name='beta', shape=[])
+        self.var_learning_rate_rise = tf.placeholder(tf.float32, name='lr', shape=[])
+        self.global_step_rise = tf.Variable(0, trainable=False, name='step')
+        # As implemented in A3C paper
+        self.n1_rise = self.conv2d_layer(self.x_rise, 8, 16, 'conv11', strides=[1, 4, 4, 1])
+        self.n2_rise = self.conv2d_layer(self.n1_rise, 4, 32, 'conv12', strides=[1, 2, 2, 1])
+        self.action_index_rise = tf.placeholder(tf.float32, [None, self.num_actions])
+        _input = self.n2_rise
+        flatten_input_shape = _input.get_shape()
+        nb_elements = flatten_input_shape[1] * flatten_input_shape[2] * flatten_input_shape[3]
+        self.flat_rise = tf.reshape(_input, shape=[-1, nb_elements._value])
+        self.d1_rise = self.dense_layer(self.flat_rise, 256, 'dense1')
+        self.logits_v_rise = tf.squeeze(self.dense_layer(self.d1_rise, 1, 'logits_v', func=None), axis=[1])
+        self.cost_v_rise = 0.5 * tf.reduce_sum(tf.square(self.y_r_rise - self.logits_v_rise), axis=0)
+        self.logits_p_rise = self.dense_layer(self.d1_rise, self.num_actions, 'logits_p', func=None)
+        if Config.USE_LOG_SOFTMAX:
+            self.softmax_p_rise = tf.nn.softmax(self.logits_p_rise)
+            self.log_softmax_p_rise = tf.nn.log_softmax(self.logits_p_rise)
+            self.log_selected_action_prob_rise = tf.reduce_sum(self.log_softmax_p_rise * self.action_index_rise, axis=1)
+            self.cost_p_1_rise = self.log_selected_action_prob_rise * (self.y_r_rise - tf.stop_gradient(self.logits_v_rise))
+            self.cost_p_2_rise = -1 * self.var_beta_rise * \
+                        tf.reduce_sum(self.log_softmax_p_rise * self.softmax_p_rise, axis=1)
+        else:
+            self.softmax_p_rise = (tf.nn.softmax(self.logits_p_rise) + Config.MIN_POLICY) / (1.0 + Config.MIN_POLICY * self.num_actions)
+            self.selected_action_prob_rise = tf.reduce_sum(self.softmax_p_rise * self.action_index_rise, axis=1)
+            self.cost_p_1_rise = tf.log(tf.maximum(self.selected_action_prob_rise, self.log_epsilon)) \
+                        * (self.y_r_rise - tf.stop_gradient(self.logits_v_rise))
+            self.cost_p_2_rise = -1 * self.var_beta_rise * \
+                        tf.reduce_sum(tf.log(tf.maximum(self.softmax_p_rise, self.log_epsilon)) *
+                                      self.softmax_p_rise, axis=1)
+        self.cost_p_1_agg_rise = tf.reduce_sum(self.cost_p_1_rise, axis=0)
+        self.cost_p_2_agg_rise = tf.reduce_sum(self.cost_p_2_rise, axis=0)
+        self.cost_p_rise = -(self.cost_p_1_agg_rise + self.cost_p_2_agg_rise)
+        if Config.DUAL_RMSPROP:
+            self.opt_p_rise = tf.train.RMSPropOptimizer(
+                learning_rate=self.var_learning_rate_rise,
+                decay=Config.RMSPROP_DECAY,
+                momentum=Config.RMSPROP_MOMENTUM,
+                epsilon=Config.RMSPROP_EPSILON)
+            self.opt_v_rise = tf.train.RMSPropOptimizer(
+                learning_rate=self.var_learning_rate_rise,
+                decay=Config.RMSPROP_DECAY,
+                momentum=Config.RMSPROP_MOMENTUM,
+                epsilon=Config.RMSPROP_EPSILON)
+        else:
+            self.cost_all_rise = self.cost_p_rise + self.cost_v_rise
+            self.opt_rise = tf.train.RMSPropOptimizer(
+                learning_rate=self.var_learning_rate_rise,
+                decay=Config.RMSPROP_DECAY,
+                momentum=Config.RMSPROP_MOMENTUM,
+                epsilon=Config.RMSPROP_EPSILON)
+        if Config.USE_GRAD_CLIP:
+            if Config.DUAL_RMSPROP:
+                self.opt_grad_v_rise = self.opt_v_rise.compute_gradients(self.cost_v_rise)
+                self.opt_grad_v_clipped_rise = [(tf.clip_by_norm(g, Config.GRAD_CLIP_NORM),v) 
+                                            for g,v in self.opt_grad_v_rise if not g is None]
+                self.train_op_v_rise = self.opt_v_rise.apply_gradients(self.opt_grad_v_clipped_rise)
+                self.opt_grad_p_rise = self.opt_p_rise.compute_gradients(self.cost_p_rise)
+                self.opt_grad_p_clipped_rise = [(tf.clip_by_norm(g, Config.GRAD_CLIP_NORM),v)
+                                            for g,v in self.opt_grad_p_rise if not g is None]
+                self.train_op_p_rise = self.opt_p_rise.apply_gradients(self.opt_grad_p_clipped_rise)
+                self.train_op_rise = [self.train_op_p_rise, self.train_op_v_rise]
+            else:
+                self.opt_grad_rise = self.opt_rise.compute_gradients(self.cost_all_rise)
+                self.opt_grad_clipped_rise = [(tf.clip_by_average_norm(g, Config.GRAD_CLIP_NORM),v) for g,v in self.opt_grad_rise]
+                self.train_op_rise = self.opt_rise.apply_gradients(self.opt_grad_clipped_rise)
+        else:
+            if Config.DUAL_RMSPROP:
+                self.train_op_v_rise = self.opt_p_rise.minimize(self.cost_v_rise, global_step=self.global_step_rise)
+                self.train_op_p_rise = self.opt_v_rise.minimize(self.cost_p_rise, global_step=self.global_step_rise)
+                self.train_op_rise = [self.train_op_p_rise, self.train_op_v_rise]
+            else:
+                self.train_op_rise = self.opt_rise.minimize(self.cost_all_rise, global_step=self.global_step_rise)
+
 
     def _create_graph(self):
         self.x = tf.placeholder(
@@ -230,6 +329,9 @@ class NetworkVP:
         return prediction
     
     def predict_p_and_v(self, x):
+        #print(self.sess_rise.run([self.softmax_p_rise, self.logits_v_rise], feed_dict={self.x_rise: x}))
+        #print("shape:", x[0].shape())
+        #exit()
         return self.sess.run([self.softmax_p, self.logits_v], feed_dict={self.x: x})
     
     def train(self, x, y_r, a, trainer_id):
@@ -266,3 +368,21 @@ class NetworkVP:
 
     def get_variable_value(self, name):
         return self.sess.run(self.graph.get_tensor_by_name(name))
+    
+    #def get_prediction_rise(self, x):
+        #print("en prediction rise")
+        #states = np.zeros(
+            #(Config.PREDICTION_BATCH_SIZE, Config.IMAGE_HEIGHT, Config.IMAGE_WIDTH, Config.STACKED_FRAMES),
+            #dtype=np.float32)
+        #print("state shapio:", states.shape)
+        #print("shape x:", x[0].shape)
+        #print("state shapio[0]:", states[0].shape)
+        ##exit()
+        #states[0] = x[0]
+        #states[1] = x[0]
+        #states[2] = x[0]
+        #states[3] = x[0]
+        #batch = states[:4]
+        #print("batch size:", batch.shape)
+        #return self.sess_rise.run([self.softmax_p_rise, self.logits_v_rise], feed_dict={self.x_rise: batch})
+
--- ga3c/ProcessAgent.py
+++ ga3c/ProcessAgent.py
@@ -34,6 +34,9 @@ from Config import Config
 from Environment import Environment
 from Experience import Experience
 
+# hierarchical
+import tensorflow as tf
+from Config import Config
 
 class ProcessAgent(Process):
     def __init__(self, id, prediction_q, training_q, episode_log_q):
@@ -53,6 +56,136 @@ class ProcessAgent(Process):
         self.wait_q = Queue(maxsize=1)
         self.exit_flag = Value('i', 0)
 
+        self.graph_rise = tf.Graph()
+        with self.graph_rise.as_default(), tf.device("cpu:0"):
+            self._create_graph_scope()
+            self.sess_rise = tf.Session(
+                graph=self.graph_rise,
+                config=tf.ConfigProto(
+                    allow_soft_placement=False,
+                    log_device_placement=False,
+                    gpu_options=tf.GPUOptions(allow_growth=True)))
+            #with self.sess_rise.as_default():
+            self.sess_rise.run(tf.global_variables_initializer())
+            vars = tf.global_variables()
+            self.saver_rise = tf.train.Saver({var.name: var for var in vars}, max_to_keep=0)
+            self.saver_rise.restore(self.sess_rise, 'checkpoints/risetotop/network_00003000')
+
+
+    def _create_graph_scope(self):
+        #with tf.name_scope("risetotop"):
+            #self._create_graph()
+            #self.saver.restore(self.sess, "checkpoints/risetotop/network_00002001")
+        self.x_rise = tf.placeholder(
+            tf.float32, [None, 168, 168, 4], name='X')
+        self.y_r_rise = tf.placeholder(tf.float32, [None], name='Yr')
+        self.var_beta_rise = tf.placeholder(tf.float32, name='beta', shape=[])
+        self.var_learning_rate_rise = tf.placeholder(tf.float32, name='lr', shape=[])
+        self.global_step_rise = tf.Variable(0, trainable=False, name='step')
+        # As implemented in A3C paper
+        self.n1_rise = self.conv2d_layer(self.x_rise, 8, 16, 'conv11', strides=[1, 4, 4, 1])
+        self.n2_rise = self.conv2d_layer(self.n1_rise, 4, 32, 'conv12', strides=[1, 2, 2, 1])
+        self.action_index_rise = tf.placeholder(tf.float32, [None, self.num_actions])
+        _input = self.n2_rise
+        flatten_input_shape = _input.get_shape()
+        nb_elements = flatten_input_shape[1] * flatten_input_shape[2] * flatten_input_shape[3]
+        self.flat_rise = tf.reshape(_input, shape=[-1, nb_elements._value])
+        self.d1_rise = self.dense_layer(self.flat_rise, 256, 'dense1')
+        self.logits_v_rise = tf.squeeze(self.dense_layer(self.d1_rise, 1, 'logits_v', func=None), axis=[1])
+        self.cost_v_rise = 0.5 * tf.reduce_sum(tf.square(self.y_r_rise - self.logits_v_rise), axis=0)
+        self.logits_p_rise = self.dense_layer(self.d1_rise, self.num_actions, 'logits_p', func=None)
+        if Config.USE_LOG_SOFTMAX:
+            self.softmax_p_rise = tf.nn.softmax(self.logits_p_rise)
+            self.log_softmax_p_rise = tf.nn.log_softmax(self.logits_p_rise)
+            self.log_selected_action_prob_rise = tf.reduce_sum(self.log_softmax_p_rise * self.action_index_rise, axis=1)
+            self.cost_p_1_rise = self.log_selected_action_prob_rise * (self.y_r_rise - tf.stop_gradient(self.logits_v_rise))
+            self.cost_p_2_rise = -1 * self.var_beta_rise * \
+                        tf.reduce_sum(self.log_softmax_p_rise * self.softmax_p_rise, axis=1)
+        else:
+            self.softmax_p_rise = (tf.nn.softmax(self.logits_p_rise) + Config.MIN_POLICY) / (1.0 + Config.MIN_POLICY * self.num_actions)
+            self.selected_action_prob_rise = tf.reduce_sum(self.softmax_p_rise * self.action_index_rise, axis=1)
+            self.cost_p_1_rise = tf.log(tf.maximum(self.selected_action_prob_rise, 1e-6)) \
+                        * (self.y_r_rise - tf.stop_gradient(self.logits_v_rise))
+            self.cost_p_2_rise = -1 * self.var_beta_rise * \
+                        tf.reduce_sum(tf.log(tf.maximum(self.softmax_p_rise, 1e-6)) *
+                                      self.softmax_p_rise, axis=1)
+        self.cost_p_1_agg_rise = tf.reduce_sum(self.cost_p_1_rise, axis=0)
+        self.cost_p_2_agg_rise = tf.reduce_sum(self.cost_p_2_rise, axis=0)
+        self.cost_p_rise = -(self.cost_p_1_agg_rise + self.cost_p_2_agg_rise)
+        if Config.DUAL_RMSPROP:
+            self.opt_p_rise = tf.train.RMSPropOptimizer(
+                learning_rate=self.var_learning_rate_rise,
+                decay=Config.RMSPROP_DECAY,
+                momentum=Config.RMSPROP_MOMENTUM,
+                epsilon=Config.RMSPROP_EPSILON)
+            self.opt_v_rise = tf.train.RMSPropOptimizer(
+                learning_rate=self.var_learning_rate_rise,
+                decay=Config.RMSPROP_DECAY,
+                momentum=Config.RMSPROP_MOMENTUM,
+                epsilon=Config.RMSPROP_EPSILON)
+        else:
+            self.cost_all_rise = self.cost_p_rise + self.cost_v_rise
+            self.opt_rise = tf.train.RMSPropOptimizer(
+                learning_rate=self.var_learning_rate_rise,
+                decay=Config.RMSPROP_DECAY,
+                momentum=Config.RMSPROP_MOMENTUM,
+                epsilon=Config.RMSPROP_EPSILON)
+        if Config.USE_GRAD_CLIP:
+            if Config.DUAL_RMSPROP:
+                self.opt_grad_v_rise = self.opt_v_rise.compute_gradients(self.cost_v_rise)
+                self.opt_grad_v_clipped_rise = [(tf.clip_by_norm(g, Config.GRAD_CLIP_NORM),v) 
+                                            for g,v in self.opt_grad_v_rise if not g is None]
+                self.train_op_v_rise = self.opt_v_rise.apply_gradients(self.opt_grad_v_clipped_rise)
+                self.opt_grad_p_rise = self.opt_p_rise.compute_gradients(self.cost_p_rise)
+                self.opt_grad_p_clipped_rise = [(tf.clip_by_norm(g, Config.GRAD_CLIP_NORM),v)
+                                            for g,v in self.opt_grad_p_rise if not g is None]
+                self.train_op_p_rise = self.opt_p_rise.apply_gradients(self.opt_grad_p_clipped_rise)
+                self.train_op_rise = [self.train_op_p_rise, self.train_op_v_rise]
+            else:
+                self.opt_grad_rise = self.opt_rise.compute_gradients(self.cost_all_rise)
+                self.opt_grad_clipped_rise = [(tf.clip_by_average_norm(g, Config.GRAD_CLIP_NORM),v) for g,v in self.opt_grad_rise]
+                self.train_op_rise = self.opt_rise.apply_gradients(self.opt_grad_clipped_rise)
+        else:
+            if Config.DUAL_RMSPROP:
+                self.train_op_v_rise = self.opt_p_rise.minimize(self.cost_v_rise, global_step=self.global_step_rise)
+                self.train_op_p_rise = self.opt_v_rise.minimize(self.cost_p_rise, global_step=self.global_step_rise)
+                self.train_op_rise = [self.train_op_p_rise, self.train_op_v_rise]
+            else:
+                self.train_op_rise = self.opt_rise.minimize(self.cost_all_rise, global_step=self.global_step_rise)
+
+
+    def dense_layer(self, input, out_dim, name, func=tf.nn.relu):
+        in_dim = input.get_shape().as_list()[-1]
+        d = 1.0 / np.sqrt(in_dim)
+        with tf.variable_scope(name):
+            w_init = tf.random_uniform_initializer(-d, d)
+            b_init = tf.random_uniform_initializer(-d, d)
+            w = tf.get_variable('w', dtype=tf.float32, shape=[in_dim, out_dim], initializer=w_init)
+            b = tf.get_variable('b', shape=[out_dim], initializer=b_init)
+
+            output = tf.matmul(input, w) + b
+            if func is not None:
+                output = func(output)
+
+        return output
+
+    def conv2d_layer(self, input, filter_size, out_dim, name, strides, func=tf.nn.relu):
+        in_dim = input.get_shape().as_list()[-1]
+        d = 1.0 / np.sqrt(filter_size * filter_size * in_dim)
+        with tf.variable_scope(name):
+            w_init = tf.random_uniform_initializer(-d, d)
+            b_init = tf.random_uniform_initializer(-d, d)
+            w = tf.get_variable('w',
+                                shape=[filter_size, filter_size, in_dim, out_dim],
+                                dtype=tf.float32,
+                                initializer=w_init)
+            b = tf.get_variable('b', shape=[out_dim], initializer=b_init)
+
+            output = tf.nn.conv2d(input, w, strides=strides, padding='SAME') + b
+            if func is not None:
+                output = func(output)
+
+        return output
     @staticmethod
     def _accumulate_rewards(experiences, discount_factor, terminal_reward):
         reward_sum = terminal_reward
@@ -70,6 +203,8 @@ class ProcessAgent(Process):
 
     def predict(self, state):
         # put the state in the prediction q
+        #print("sssstate:", state.shape)
+        #exit()
         self.prediction_q.put((self.id, state))
         # wait for the prediction to come back
         p, v = self.wait_q.get()
@@ -102,6 +237,15 @@ class ProcessAgent(Process):
             reward_sum += reward
             exp = Experience(self.env.previous_state, action, prediction, reward, done)
             experiences.append(exp)
+
+            states = np.zeros(
+                (Config.PREDICTION_BATCH_SIZE, Config.IMAGE_HEIGHT,
+                 Config.IMAGE_WIDTH, Config.STACKED_FRAMES),
+                dtype=np.float32)
+            states[0] = self.env.current_state
+            batch = states[:1]
+            print("shape state:", states.shape)
+            print("prediction:", self.sess_rise.run(self.softmax_p_rise, feed_dict={self.x_rise: batch}))
 
             if done or time_count == Config.TIME_MAX:
                 terminal_reward = reward if done else value
